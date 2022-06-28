3e7904454a06de49b03b3b675a907d3f4f62709f
Danny Maksoud <dannymaksoud@gmail.com>
2022-06-28 16:00:32 +0100

    Array to ArrayList for both Strings and Integers

--- ArrToArrList.java
+++ ArrToArrList.java
@@ -1,3 +1,10 @@
-public static void arrToArrList() {
-		String[] ArrName = {"a", "b", "c", "d", "e"};  
+public static void arrToArrListString() { 
+		String[] ArrName = {"a", "b", "c", "d", "e"};  //String Array to ArrayList Conversion
 		ArrayList<String> ArrListName = new ArrayList<>(Arrays.asList(ArrName));
+
+public static void arrToArrListInteger() {
+		Integer[] ArrName = {1, 2, 4, 4, 5};  
+		ArrayList<Integer> ArrListName = new ArrayList<>(Arrays.asList(ArrName));
+		
+		for(Integer i : ArrListName) 
+			System.out.println(i);
